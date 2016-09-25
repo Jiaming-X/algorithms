5df16ea9db83726521bf9fbe4d9d50f2fe06e703
brucexiejiaming <brucexiejiaming@gmail.com>
2016-09-25 10:23:25 -0400

    update

--- leetcodePractice/src/main/java/class99/solution.java
+++ leetcodePractice/src/main/java/class99/solution.java
@@ -2,8 +2,14 @@ package class99;
 
 public class solution {
     public static void main(String[] args) {
-        reverseWordsInString soln = new reverseWordsInString();
-        System.out.println("|" + soln.reverseWords("    an   apple    ") + "|");
+//        reverseWordsInString soln = new reverseWordsInString();
+//        System.out.println("|" + soln.reverseWords("    an   apple    ") + "|");
+//        String str = "hello_world@h";
+//        System.out.println(str.split("@").length);
+        double diff = 100.00;
+        StringBuilder sb = new StringBuilder();
+        sb.append(diff);
+        System.out.println(sb.toString());
     }
 
 }
